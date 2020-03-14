3e32a2921028632456900da3f758970df2105934
StephC <stephaniebchiang@gmail.com>
2020-03-14 16:46:20 -0400

    contacts name rendering

--- components/FriendsList.js
+++ components/FriendsList.js
@@ -2,57 +2,121 @@ import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import * as Contacts from 'expo-contacts';
 import styles from '../styles';
-import { ScrollView, State } from 'react-native-gesture-handler';
+import { ScrollView } from 'react-native-gesture-handler';
 
-export const FriendsList = () => {
-  let [nameNum, setNameNum] = React.useState([]);
+// export const FriendsList = () => {
+//   let [nameNum, setNameNum] = React.useState([]);
 
-  // state = {
-  //   nameNum: []
-  // }
+//   useEffect(() => {
+//     (async () => {
+//       const { status } = await Contacts.requestPermissionsAsync();
+//       if (status === 'granted') {
+//         const { data } = await Contacts.getContactsAsync({
+//           fields: [
+//             Contacts.Fields.Name,
+//             Contacts.Fields.PhoneNumbers
+//           ],
+//         })
+//         setNameNum({
+//           nameNum: data.map(contact => {
+//             let contObj = {}
+//             contObj.name = contact.name
+//             contObj.number = contact.phoneNumbers[0].number
+//             return contObj
+//           })
+//         })
+//       }
+//     })();
+//   }, []);
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Contacts.requestPermissionsAsync();
-      if (status === 'granted') {
-        const { data } = await Contacts.getContactsAsync({
-          fields: [
-            Contacts.Fields.Name,
-            Contacts.Fields.PhoneNumbers
-          ],
-        });
-        // console.log(data)
-        setNameNum({
-          nameNum: data
+//   console.log(nameNum)
+
+//   if(nameNum.length) {
+//     return (
+//       <View style={styles.view}>
+//         <Text style={styles.Text}>Contacts</Text>
+//         <ScrollView>
+//           {nameNum.forEach(contact => {
+//             return (
+//               <Text style={styles.Text}>
+//                 {contact.name}
+//               </Text>
+//             )
+//           })}
+//         </ScrollView>
+//       </View>
+//     );
+//   } else {
+//     return (
+//       <View style={styles.view}>
+//         <Text style={styles.Text}>
+//           You have no friends :(
+//         </Text>
+//       </View>
+//     )
+//   }
+// }
+
+export class FriendsList extends React.Component {
+  state = {
+    nameNum: []
+  }
+
+  async componentDidMount(){
+    this.getNameNums()
+  }
+
+  getNameNums = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [
+          Contacts.Fields.Name,
+          Contacts.Fields.PhoneNumbers
+        ],
+      })
+      this.setState({
+        nameNum: data.map(contact => {
+          let contObj = {}
+          contObj.name = contact.name
+          contObj.number = contact.phoneNumbers[0].number
+          return contObj
         })
-        // data.forEach(contact => {
-        //   let contObj = {}
-        //   contObj[contact.name] = contact.phoneNumbers[0].number
-        //   // console.log(nameNum)
-        //   setNameNum({
-        //     nameNum: [...nameNum, contObj]
-        //   })
-        // })
+      })
+    }
+  }
 
-        console.log(nameNum)
+  render(){
+    // console.log(this.state)
+    // return (
+    //   <View style={styles.view}>
+    //     <Text style={styles.Text}>FRIENDSHIP IS MAGIC</Text>
+    //   </View>
+    // )
 
-        // console.log(typeof data)
-      }
-    })();
-  }, []);
-
-  return (
-    <View style={styles.view}>
-      <Text style={styles.Text}>Contacts</Text>
-      {/* <ScrollView>
-        {nameNum.forEach(contact => {
-          return (
-            <Text style={styles.Text}>
-              hello
-            </Text>
-          )
-        })}
-      </ScrollView> */}
-    </View>
-  );
+    if(this.state.nameNum.length) {
+      return (
+        <View style={styles.view}>
+          <Text style={styles.Text}>Contacts</Text>
+          <ScrollView>
+            {this.state.nameNum.map(contact => {
+              return (
+                <Text style={styles.Text} key={contact.number}>
+                  {contact.name}
+                </Text>
+              )
+            })}
+          </ScrollView>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.view}>
+          <Text style={styles.Text}>
+            You have no friends :(
+          </Text>
+        </View>
+      )
+    }
+  }
 }

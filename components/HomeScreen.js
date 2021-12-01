a6e6251ac910059da977016e997160a26497ecbd
StephC <stephaniebchiang@gmail.com>
2021-12-01 15:07:29 -0500

    Added: airhorn sound

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Pressable } from 'react-native';
+import { Audio } from 'expo-av';
 import styles from '../styles';
 
 export const HomeScreen = ({ navigation }) => {
+  const [sound, setSound] = useState();
+
+  const playSound = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require('../assets/airhorn.mp3')
+    );
+    setSound(sound);
+    await sound.playAsync();
+  };
+
+  useEffect(() => {
+    return sound ? () => sound.unloadAsync() : undefined;
+  }, [sound]);
+
   return (
     <View style={styles.view} >
 
@@ -12,6 +27,10 @@ export const HomeScreen = ({ navigation }) => {
 
       <Pressable style={styles.button} onPress={() => navigation.navigate('FRIENDS')}>
         <Text style={styles.buttonText}>FIND FRIENDS</Text>
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={() => playSound()}>
+        <Text style={styles.buttonText}>PLAY AIR HORN</Text>
       </Pressable>
 
     </View>
